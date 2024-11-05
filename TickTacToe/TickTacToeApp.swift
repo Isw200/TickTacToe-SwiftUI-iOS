0c099abd10e2130fa0c3129397353758c1d5b9bb
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2024-11-05 12:53:54 +0530

    Initial Commit

---
+++ TickTacToe/TickTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTacToeApp.swift
+//  TickTacToe
+//
+//  Created by Isuru Ariyarathna on 2024-11-05.
+//
+
+import SwiftUI
+
+@main
+struct TickTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TickTacToeTests/TickTacToeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TickTacToeTests.swift
+//  TickTacToeTests
+//
+//  Created by Isuru Ariyarathna on 2024-11-05.
+//
+
+import Testing
+@testable import TickTacToe
+
+struct TickTacToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
